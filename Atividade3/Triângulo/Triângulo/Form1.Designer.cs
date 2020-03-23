a912eb150494d6f50d697a11aa574d4fb3e0ff00
mayumi-fe <62565534+mayumi-fe@users.noreply.github.com>
2020-03-23 15:18:21 -0300

    Add files via upload

---
+++ Atividade3/Triângulo/Triângulo/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Triângulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVERIFICAR = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnVERIFICAR
+            // 
+            this.btnVERIFICAR.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVERIFICAR.Location = new System.Drawing.Point(79, 196);
+            this.btnVERIFICAR.Name = "btnVERIFICAR";
+            this.btnVERIFICAR.Size = new System.Drawing.Size(120, 28);
+            this.btnVERIFICAR.TabIndex = 0;
+            this.btnVERIFICAR.Text = "VERIFICAR";
+            this.btnVERIFICAR.UseVisualStyleBackColor = true;
+            this.btnVERIFICAR.Click += new System.EventHandler(this.btnVERIFICAR_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Verificação - Triângulo";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(41, 75);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(59, 19);
+            this.lblA.TabIndex = 2;
+            this.lblA.Text = "Valor A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblB.Location = new System.Drawing.Point(41, 110);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(60, 19);
+            this.lblB.TabIndex = 3;
+            this.lblB.Text = "Valor B";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblC.Location = new System.Drawing.Point(41, 152);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(60, 19);
+            this.lblC.TabIndex = 4;
+            this.lblC.Text = "Valor C";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(139, 76);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 5;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(139, 111);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 6;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(139, 153);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 245);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVERIFICAR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVERIFICAR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+    }
+}
+
---
+++ Atividade3/Triângulo/Triângulo/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triângulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVERIFICAR_Click(object sender, EventArgs e)
+        {
+            if(txtA.Text=="" && txtB.Text=="" && txtC.Text=="")
+            {
+                MessageBox.Show("Preencher todos os campos", "ALERTA");
+            }
+
+            else
+            {
+                double valorA, valorB, valorC;
+                if (double.TryParse(txtA.Text, out valorA) && double.TryParse(txtB.Text, out valorB) && double.TryParse(txtC.Text, out valorC))
+                {
+                    if (((Math.Abs(valorB - valorC)) < valorA && valorA < (valorB + valorC)) && ((Math.Abs(valorA - valorC)) < valorB && valorB < (valorA + valorC)) && ((Math.Abs(valorA - valorB)) < valorC && valorC < (valorA + valorC)))
+                    {
+                        if(valorA!=valorB && valorA!=valorC && valorB!=valorC)
+                        {
+                            MessageBox.Show("Triângulo escaleno", "Triângulo");
+                        }
+                        else
+                        {
+                            if(valorA == valorB && valorA == valorC && valorB == valorC)
+                            {
+                                MessageBox.Show("Triângulo equilátero", "Triângulo");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Triângulo isósceles", "Triângulo");
+                            }
+                        }
+
+                    }
+                    else
+                        MessageBox.Show("Valores não pertencem ao lado de um triângulo", "AVISO");
+                }
+            }
+        }
+    }
+}
